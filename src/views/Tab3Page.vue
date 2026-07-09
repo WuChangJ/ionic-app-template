627dadf14ab1230f5bb8af01faf0cd3aa8a1a001
WuChangJ <1141077445@qq.com>
2026-07-09 14:54:42 +0800

    Initial commit

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -12,11 +12,17 @@
         </ion-toolbar>
       </ion-header>
 
-      <ion-button>去往二级页面</ion-button>
+      <ion-button @click="goToSafetyCenter">去往安全中心页面</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goToSafetyCenter = () => {
+  router.push('/safetyCenter');
+};
 </script>
---
+++ src/views/safetyCenter/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>安全中心</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">安全中心</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div>修改登录密码</div>
+      <div>设置头像</div>
+      <div>修改昵称</div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
